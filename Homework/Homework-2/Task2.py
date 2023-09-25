e3824682f82223382f84f32055035ea3b17a5fa2
Denys Shvets <shvecdn@gmail.com>
2023-09-25 21:56:04 +0300

    Completed

---
+++ Homework/Homework-2/Task2.py
@@ -0,0 +1,20 @@
+# Hello world
+print("Hello world")
+
+# Great___day
+print("Great", "day", sep='___' )
+
+'''
+Hello 
+
+World
+'''
+print("Hello \n\nWorld")
+'''
+Hello
+
+World
+'''
+#Example2
+print("Hello", end='\n'*2)
+print("World")
---
+++ Homework/Homework-2/Task3.py
@@ -0,0 +1,12 @@
+# character "O"
+print("#"*9)
+print("#\t\t#")
+print("#\t\t#")
+print("#\t\t#")
+print("#"*9, end='\n\n') #Gap after "O"
+# character "H"
+print("#\t\t#")
+print("#\t\t#")
+print("#"*9)
+print("#\t\t#")
+print("#\t\t#")
